9cd3546548b3b7f3565d19d9782508bb8ae5c5f7
mi2507 <mreginadasilva@yahoo.com.br>
2021-09-13 17:41:55 -0300

    correção espacos e codigo comentado

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function media() {
-    var notaUm =  parseFloat(document.getElementById("valorUm").value);
-
+    var notaUm = parseFloat(document.getElementById("valorUm").value);
     var notaDois = parseFloat(document.getElementById("valorDois").value);
     var media = (notaUm + notaDois) / 2;
-    if(media >= 6){
-     return document.getElementById("aprovado").innerHTML = (media) + " Aluno aprovado!";
-    }else(media < 6)
+    if (media >= 6) {
+        return document.getElementById("aprovado").innerHTML = (media) + " Aluno aprovado!";
+    } else (media < 6)
     return document.getElementById("reprovado").innerHTML = (media) + " Aluno reprovado!";
-
-  }
+}
