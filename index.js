90cf6d6e3f7d03954856e7e0c96e3c055ff4b3a6
mi2507 <mreginadasilva@yahoo.com.br>
2021-09-13 17:31:57 -0300

    projeto primeira aula alura imersão dev

---
+++ index.js
@@ -0,0 +1,11 @@
+function media() {
+    var notaUm =  parseFloat(document.getElementById("valorUm").value);
+
+    var notaDois = parseFloat(document.getElementById("valorDois").value);
+    var media = (notaUm + notaDois) / 2;
+    if(media >= 6){
+     return document.getElementById("aprovado").innerHTML = (media) + " Aluno aprovado!";
+    }else(media < 6)
+    return document.getElementById("reprovado").innerHTML = (media) + " Aluno reprovado!";
+
+  }
